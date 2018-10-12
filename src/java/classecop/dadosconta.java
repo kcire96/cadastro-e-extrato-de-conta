822906570e4e5d7a53ebcd093cedaafd00d54c0b
catie <catie@LAPTOP-G2A8BQKA>
2018-10-12 12:38:48 -0300

    classe conta  para coleta cpf,numero da conta

---
+++ src/java/classecop/dadosconta.java
@@ -0,0 +1,39 @@
+
+package classecop;
+
+
+public class dadosconta {
+       private int conta;
+    private String numerodaconta;
+    private String cpfdotitular;
+
+
+    public int getconta() {
+        return conta;
+    }
+
+    public void setconta(int conta) {
+        this.conta = conta;
+    }
+
+    public String getNumerodaconta() {
+        return numerodaconta;
+    }
+
+    public void setNumerodaconta(String numerodaconta) {
+        this.numerodaconta = numerodaconta;
+    }
+
+    public String getCpfdotitular() {
+        return cpfdotitular;
+    }
+
+
+    public void setCpfdotitular(String cpfdotitular) {
+        this.cpfdotitular = cpfdotitular;
+    }
+    
+}
+
+    
+
